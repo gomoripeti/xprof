1afdda4c05b69e55c44ea81e7e63052430c39343
Péter Gömöri <gomoripeti@gmail.com>
2019-12-31 14:06:51 +0100

    Use atomics in argdist if available

--- apps/xprof_core/src/xprof_core_cmd_argdist.erl
+++ apps/xprof_core/src/xprof_core_cmd_argdist.erl
@@ -58,18 +58,30 @@ get_cmd_id(Options) ->
 
 %% tracer
 
--record(state, {freq_count = dict:new(),
-                all_keys = [],
-                max_enum}).
-
 init(Options, _MFASpec) ->
     MaxEnum = proplists:get_value(enum, Options, 10),
+    init_state(MaxEnum).
+
+handle_event({trace_ts, _Pid, call, _MFA, Args, _StartTime}, _, State) ->
+    update_count(Args, State);
+handle_event(_, _, _) ->
+    ok.
+
+-ifdef(no_atomics).
+%% Old implementation with counters in a dictionary
+
+-record(state, {freq_count = dict:new() :: dict:dict(term(), pos_integer()),
+                all_keys = [] :: list(), %% keys in the order of arrival
+                max_enum}).
+
+-spec init_state(pos_integer()) -> #state{}.
+init_state(MaxEnum) ->
     {ok, #state{max_enum = MaxEnum}}.
 
-handle_event({trace_ts, _Pid, call, _MFA, Args, _StartTime}, _,
-             State = #state{freq_count = FreqCount,
-                            all_keys = AllKeys,
-                            max_enum = MaxEnum}) ->
+-spec update_count(list(), #state{}) -> {ok, #state{}}.
+update_count(Args, State = #state{freq_count = FreqCount,
+                                  all_keys = AllKeys,
+                                  max_enum = MaxEnum}) ->
     {Key, NewAllKeys} =
         case lists:member(Args, AllKeys) of
             true ->
@@ -89,10 +101,7 @@ handle_event({trace_ts, _Pid, call, _MFA, Args, _StartTime}, _,
         end,
     {ok, State#state{
            freq_count = dict:update_counter(Key, 1, FreqCount),
-           all_keys = NewAllKeys}};
-handle_event(_, _, _) ->
-    ok.
-
+           all_keys = NewAllKeys}}.
 
 take_snapshot(State = #state{freq_count = FreqCount,
                              all_keys = AllKeys}) ->
@@ -102,3 +111,61 @@ take_snapshot(State = #state{freq_count = FreqCount,
                     end
                     || Key <- lists:reverse(AllKeys)],
     {IntervalList, State#state{freq_count = dict:new()}}.
+
+-else.
+%% New implementation with atomic counters
+
+-record(state, {freq_count :: atomics:atomics_ref(),
+                key_to_idx = #{} :: #{term() => pos_integer()}, %% #{key => counter_idx}
+                all_keys = [] :: list(), %% keys in the order of arrival
+                max_idx :: pos_integer()}).
+
+-spec init_state(pos_integer()) -> #state{}.
+init_state(MaxEnum) ->
+    %% 1 extra slot for current max idx, and another for the `other' bucket
+    MaxIdx = MaxEnum + 2,
+    Counters = atomics:new(MaxIdx, []),
+    atomics:put(Counters, 1, 1), %% current max idx
+    {ok, #state{freq_count = Counters,
+                max_idx = MaxIdx}}.
+
+-spec update_count(list(), #state{}) -> {ok, #state{}}.
+update_count(Args, State = #state{freq_count = Counters,
+                                  key_to_idx = KeyToIdx,
+                                  all_keys = AllKeys,
+                                  max_idx = MaxIdx}) ->
+    case maps:find(Args, KeyToIdx) of
+        {ok, Idx} ->
+            atomics:add(Counters, Idx, 1),
+            {ok, State};
+        error ->
+            case AllKeys of
+                ['other'|_] ->
+                    atomics:add(Counters, MaxIdx, 1),
+                    {ok, State};
+                _ ->
+                    case atomics:add_get(Counters, 1, 1) of
+                        MaxIdx ->
+                            %% reached max, have to add the `other' bucket
+                            atomics:add(Counters, MaxIdx, 1),
+                            {ok, State#state{all_keys = ['other'|AllKeys]}};
+                        NewIdx ->
+                            atomics:add(Counters, NewIdx, 1),
+                            {ok, State#state{key_to_idx = KeyToIdx#{Args => NewIdx},
+                                             all_keys = [Args|AllKeys]}}
+                    end
+            end
+    end.
+
+take_snapshot(#state{freq_count = Counters,
+                     all_keys = AllKeys}) ->
+    CurrentMaxIdx = atomics:get(Counters, 1),
+    snapshot_loop(AllKeys, CurrentMaxIdx, Counters, []).
+
+snapshot_loop([], Idx, _, Acc) ->
+    1 = Idx, %% assert
+    Acc;
+snapshot_loop([Key|AllKeys], Idx, Counters, Acc) ->
+    snapshot_loop(AllKeys, Idx - 1, Counters, [{Key, atomics:exchange(Counters, Idx, 0)}|Acc]).
+
+-endif.
